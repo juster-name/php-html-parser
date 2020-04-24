e9a6fd17ef798968e5cb877532825fef89dfe108
juster-name <vladlen044@gmail.com>
2020-04-24 10:35:36 +0300

    test case

--- test.php
+++ test.php
@@ -7,246 +7,115 @@ require ('filters.php');
 
 $internal = [];
 $external = [];
+$imgs = [];
 
-$url = "https://www.uavi.top/login.php/";
-
-
-
-$hrefFilter =  new HrefFilter($url);
-$htmlCrawler = new HtmlCrawler();
-
-$aHrefOption = new Option('a', null, 
-    [new Option('href', $hrefFilter)]);
+$url = "https://google.com.ua/";
 
 use Test\Parser\ParserSettings as PS;
+$p = new HtmlParser([], PS::Recursive);
 
-$p = new HtmlParser($htmlCrawler, [$aHrefOption], PS::Recursive);
+//$hrefFilter =  new HrefFilter($url);
+$saveImgAction= new InvokeActionParam(function($msg) use (&$imgs, &$internal, &$p)
+{     
+    //$fullUrl = $p->hrefFilter->run($urlArg);
+    $value = $msg->value;
+    
+    $tagName = $msg->option->getParent()->getValue() ?: '';
+    $attrName = $msg->option->getValue();
+
+    if ($tagName == 'img' && $attrName == 'src')
+    {
+        if (key_exists($value, $imgs))
+        {
+            return;
+        }
+        _logInFile($value, 'imgs.txt');
+        $imgs[$value] = '';
+    }
+
+});
+
+
+$imgOpts = new Option('img', null, new Option('src', new ImgFilter($p)));
+
+$p->addOptions($imgOpts);
+
 $logAction = new UserCallActionParam("Test\\Parser\\_log");
 
-$saveInternalAction = new InvokeActionParam(function($url) use (&$internal, &$hrefFilter)
+$saveInternalAction = new InvokeActionParam(function($urlArg) use (&$internal, &$p)
 { 
-    if ($hrefFilter->getUrlHost() == parse_url($url, PHP_URL_HOST))
+    $initPath = $p->getPath();
+
+    if (parse_url($initPath, PHP_URL_HOST) == parse_url($urlArg, PHP_URL_HOST))
     {
-        array_push($internal, $url);
+        if (key_exists($urlArg, $internal))
+        {
+            return;
+        }
+        _log($urlArg);
+        _logInFile($urlArg, 'internals.txt');
+        $internal[$urlArg] = '';
     }
 });
 
-$saveExternalAction = new InvokeActionParam(function($url) use (&$external, &$hrefFilter)
+$saveExternalAction = new InvokeActionParam(function($urlArg) use (&$external, &$p)
 {     
-    if ($hrefFilter->getUrlHost() != parse_url($url, PHP_URL_HOST))
+    if (parse_url($p->getPath(), PHP_URL_HOST) != parse_url($urlArg, PHP_URL_HOST))
     {
-        array_push($external, $url);
+        if (key_exists($urlArg, $external))
+        {
+            return;
+        }
+        _log($urlArg);
+        _logInFile($urlArg, 'externals.txt');
+        $external[$urlArg] = '';
     }
 });
 
-$p->onLog->add($logAction);
-
-$p->onFilterSuccess->add($saveInternalAction);
-$p->onFilterSuccess->add($saveExternalAction);
+//$p->onStart->add($logAction);
+//$p->onEnd->add($logAction);
+$p->onFilterSuccess->add($saveImgAction);
+$p->onNewUrlFound->add($saveInternalAction);
+$p->onNewUrlFound->add($saveExternalAction);
 
 $p->parse($url);
 
 echo "\n\n INTERNALS: ";
-foreach ($internal as $url)
+foreach ($internal as $url => $val)
 {
     echo "\n$url";
 }
 
 echo "\n\n EXTERNALS: ";
-foreach ($external as $url)
+foreach ($external as  $url => $val)
 {
     echo "\n$url";
 }
-/*
-$recursiveAction = new InvokeActionParam(function ($url) use ($p)
+
+echo "\n\n IMAGES: ";
+foreach ($imgs as  $url => $val)
 {
-    $domain = urlToDomain($p->getPath());
+    echo "\n$url";
+}
 
-    if (strpos($url, $domain) !== false) 
-    {
-        $p->parse($url);
-    }
-});
 
-$p->onFilterSuccess->add($recursiveAction);
-$p->parse($url);
-
-function urlToDomain($url) {
-    return implode(array_slice(explode('/', preg_replace('/https?:\/\/(www\.)?/', '', $url)), 0, 1));
- }
-
- */
-//$p->onStart->add(new UserCallActionParam("\\lol"));
-//$p->onStart->add(new TestFindAction());
-//$p->parse("https://google.com");
-
-/*
-$domain = "https://translate.google.com.ua/";
-$domainShort = "translate.google.com.ua";
-
-$queue = [];
-$img = [];
-$internal = [];
-$external = [];
-$error = [];
-array_push($queue, $domain);
-array_push($internal, $domain);
-*/
-
-function _logInFile($val)
+function _logInFile($val, $path = 'log.txt')
 {
     $curDir = dirname(__FILE__);
 
-    $fname = "log.txt";
+    $fname = $path;
     
+    //$fullVal = date("H:i:s") . ": " . $val . "\r\n"; 
 
-    $fullVal = date("H:i:s") . ": " . $val . "\r\n"; 
-
-    file_put_contents($curDir . "/$fname", $fullVal, FILE_APPEND);
+    file_put_contents($curDir . "/$fname", $val . "\r\n", FILE_APPEND);
 }
 
 function _log($val)
 {
     $fname = "log.txt";
-    _logInFile($val, $fname);
+    //_logInFile($val, $fname);
 
     $fullVal = date("H:i:s") . ": " . $val . "\r\n"; 
     echo $fullVal; 
 }
-/*
-function parse($tagsAtts)
-{
-    global $document;
-    global $domain;
-    global $domainShort;
-    global $internal;
-    global $external;
-
-    foreach ($tagsAtts as $tagName => $att)
-    {
-        _log("Looping through <$tagName>");
-
-        foreach($att as $attOpt)
-        {
-            parseAtt($tagName, $attOpt['name'], $attOpt['filter']);
-        }
-
-        try
-        {
-            $allATags = $document->getElementsByTagName("a");
-        }
-        catch(Exception $e)
-        {
-            array_push($error, "ERROR in getting <a> tag: " . $e);
-            _log("ERROR in getting <a> tag (logged)");
-        }
-
- 
-        
-    }
-}
-function parseAtt($tagName, $attName, $findFunc)
-{
-    $att = $tagName->getAttribute($attName);
-
-    if (empty($att))
-    {
-        return false;
-    }
-
-    call_user_func($findFunc, $att);
-}
-
-function findHref($href)
-{
-    global $domain;
-    global $domainShort;
-    global $internal;
-    global $external;
-    
-    if (filter_var($href, FILTER_VALIDATE_URL) === false) 
-    {
-        $href = $domain . "/" . $href;
-    }
-
-    _log("\t".$href);
-
-    if (array_search($href, $internal) || array_search($href, $external)) 
-    {
-        _log("\t\tAlready saved, continue");
-        continue;
-    }
-
-    if (strpos($href, $domainShort) !== false) 
-    {
-        _log("\t\tSaving in internal");
-        array_push($queue, $href);
-        array_push($internal, $href);
-    } 
-    else 
-    {
-        _log("\t\tSaving in external");
-        array_push($external, $href);
-    }
-}
-
-_log("--------START--------\r\n");
-
-try
-{
-    while (empty($queue) === false) 
-    {
-        $url = array_pop($queue);
-
-        try
-        {
-            _log("\r\n\r\nCreating DOM and loading HTML file: ");
-            $document = new DOMDocument();
-            $loaded = @$document->loadHTMLFile($url);
-            _log("OK.\r\n");
-        }
-        catch (Exception $e)
-        {
-            _log("ERROR (Logged)\r\n");
-            array_push($error, "DOM load html exception. Url: $url\r\n$e");
-            continue;
-        }
-        parse(
-            ['a'=> 
-                ['href', 'findHref'],
-            ]);
-
-        
-    }
-}
-catch(Exception $e)
-{
-    array_push($error, "Queue loop:" . $e);
-    _log("Unexpected ERROR occurred (logged)");
-}
-
-_log("\r\n--------END--------");
-
-
-_logInFile("-------START--------","internal.txt");
-foreach ($internal as $element) 
-{
-    _logInFile($element, "internal.txt");
-}
-_logInFile("-------END--------","internal.txt");
-
-_logInFile("-------START--------","external.txt");
-foreach ($external as $element) 
-{
-    _logInFile($element, "external.txt");
-}
-_logInFile("-------END--------","external.txt");
-
-_logInFile("-------START--------","error.txt");
-foreach ($error as $element)
-{
-    _logInFile($element, "error.txt");
-}
-_logInFile("-------END--------","error.txt");
-*/
 ?>
